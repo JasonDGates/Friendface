1061f6ff03d50ae08fa5e33a1876b04c092f2a0c
Jason Gates <gatesjasond@gmail.com>
2022-12-11 03:43:44 -0600

    friends routes completed

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
 import { register } from './controllers/auth.js'
 
 // MIDDLEWARE
@@ -44,6 +45,8 @@ app.post('auth/register', upload.single('picture', register));
 
 // Routes
 app.use('auth', authRoutes);
+app.use('/users', userRoutes);
+
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
